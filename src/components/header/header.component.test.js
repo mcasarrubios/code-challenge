8ebd92f15f99659338c33a0d69ccb051286a4aa7
Garcia Casarrubios <miguel@codeback.es>
2019-03-28 16:29:37 +0100

    fixed tests

--- src/components/header/header.component.test.js
+++ src/components/header/header.component.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Enzyme, {mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Header from './header.component';
@@ -8,7 +9,7 @@ Enzyme.configure({adapter: new Adapter()});
 describe('Header component', () => {
   it('shows the title', () => {
     const expected = 'Awesome title';
-    const wrapper = mount(<Header title={expected}/>);
+    const wrapper = mount(<Router><Header title={expected}/></Router>);
     const title = wrapper.find('.header-title');
     expect(title.text()).toBe(expected);
   });
